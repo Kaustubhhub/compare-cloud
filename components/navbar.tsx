4db7985898a6c73b27d6a855d767400236f86973
Kaustubh Kumbhare <kaustubh.kumbhare@ibm.com>
2024-12-06 16:42:38 +0530

    tables init

---
+++ components/navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+      <div className="border p-4 flex justify-center w-6/12 bg-opacity-50 backdrop-blur-lg shadow-lg border-white/30 rounded-lg">
+        <h1 className="text-2xl font-semibold  font-sans">
+          Comparison of Public Cloud Providers
+        </h1>
+      </div>
+    );
+  };
+  
+  export default Navbar;
+  
