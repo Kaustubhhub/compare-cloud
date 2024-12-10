dbd6f9b2f9977e6ce1b2147258d82bcc49ff7963
kaustubh <kaustubhkumbhare02@gmail.com>
2024-12-10 15:41:25 +0530

    updated navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,43 @@
-const Navbar = () => {
-    return (
-      <div className="border p-4 flex justify-center w-6/12 bg-opacity-50 backdrop-blur-lg shadow-lg border-white/30 rounded-lg">
-        <h1 className="text-2xl font-semibold  font-sans">
-          Comparison of Public Cloud Providers
-        </h1>
-      </div>
-    );
+"use client"
+import { useState } from 'react';
+
+const Navbar: React.FC = () => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+  // Toggle dark/light mode
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    if (isDarkMode) {
+      document.body.classList.remove('dark');
+    } else {
+      document.body.classList.add('dark');
+    }
   };
-  
-  export default Navbar;
-  
+
+  return (
+    <div className="border p-4 flex justify-between items-center w-full bg-opacity-50 backdrop-blur-lg shadow-lg border-white/30 rounded-lg">
+      <h1 className="text-2xl font-semibold font-sans">
+        Comparison of Public Cloud Providers
+      </h1>
+
+      <div className="flex items-center space-x-10">
+        {/* GitHub SVG Icon */}
+        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
+          </svg>
+
+        </a>
+
+        {/* Mail SVG Icon */}
+        <a href="mailto:someone@example.com">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
